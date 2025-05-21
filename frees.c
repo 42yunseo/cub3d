9fc6426b92a58fd4854844a68df8da262d67221b
42yunseo <yunseo@student.42gyeongsan.kr>
2025-05-21 16:27:13 +0900

    free 수정

--- frees.c
+++ frees.c
@@ -12,21 +12,21 @@
 
 #include "cub3d.h"
 
-void    free_info(t_info *info)
+void	free_info(t_info *info)
 {
-    free(info->north_path);
-    free(info->south_path);
-    free(info->west_path);
-    free(info->east_path);
-    token_free(info->map);
-    free(info);
+	free(info->north_path);
+	free(info->south_path);
+	free(info->west_path);
+	free(info->east_path);
+	token_free(info->map);
+	free(info);
 }
 
-void    free_vars(t_vars *vars)
+void	free_vars(t_vars *vars)
 {
-    free(vars->player);
-    if (vars->info != NULL)
-        free_info(vars->info);
-    mlx_destroy_window(vars->mlx, vars->win);
-    free(vars->mlx);
+	free(vars->player);
+	if (vars->info != NULL)
+		free_info(vars->info);
+	mlx_destroy_window(vars->mlx, vars->win);
+	free(vars->mlx);
 }
