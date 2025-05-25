f36a2bf445862cbcac94f09e2fb0cf6d440b5148
42yunseo <yunseo@student.42gyeongsan.kr>
2025-05-25 19:11:07 +0900

    방향문제 수정

--- player.c
+++ player.c
@@ -111,16 +111,28 @@ void	move_func(t_vars *vars, t_player *p, int direction)
 // 	render(vars);
 // }
 
-void	set_direction(t_player *player, char dir)
+void	set_direction(t_player *p, char dir)
 {
 	if (dir == 'N')
-		player->dir_y = 1.0;
+	{
+		p->dir_y = 1.0;
+		p->plane_x = 0.66;
+	}
 	if (dir == 'S')
-		player->dir_y = -1.0;
+	{
+		p->dir_y = -1.0;
+		p->plane_x = -0.66;
+	}
 	if (dir == 'E')
-		player->dir_x = 1.0;
+	{
+		p->dir_x = 1.0;
+		p->plane_y = -0.66;
+	}
 	if (dir == 'W')
-		player->dir_x = -1.0;
+	{
+		p->dir_x = -1.0;
+		p->plane_y = 0.66;
+	}
 }
 
 void	set_position(t_vars *vars, t_player *player)
@@ -150,14 +162,12 @@ void	set_position(t_vars *vars, t_player *player)
 
 t_player	*player_init(t_vars *vars)
 {
-	t_player	*player;
+	t_player	*p;
 
-	player = (t_player *)malloc(sizeof(t_player));
-	if (player == NULL)
+	p = (t_player *)malloc(sizeof(t_player));
+	if (p == NULL)
 		exit_game(vars);
-	ft_memset(player, 0, sizeof(t_player));
-	set_position(vars, player);
-	player->plane_x = 1;
-	player->plane_y = 0;
-	return (player);
+	ft_memset(p, 0, sizeof(t_player));
+	set_position(vars, p);
+	return (p);
 }
